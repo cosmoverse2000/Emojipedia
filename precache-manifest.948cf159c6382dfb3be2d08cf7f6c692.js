ef979dccf8b078ddb7f4432513780ed4ccdd2f9f
Anshul Patel <anshulpatel5757@gmail.com>
2022-07-30 09:53:17 +0530

    Updates

---
+++ precache-manifest.948cf159c6382dfb3be2d08cf7f6c692.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b97fa44d27c5cd2c43500ecac15b59",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "ee6b5caac71271137dcb",
+    "url": "/Emojipedia/static/js/2.79de99e3.chunk.js"
+  },
+  {
+    "revision": "b0778cbf5e10ee54e4d1",
+    "url": "/Emojipedia/static/js/main.f73426c8.chunk.js"
+  },
+  {
+    "revision": "8d3fb7fd0253540c73ed",
+    "url": "/Emojipedia/static/js/runtime-main.3c6b51aa.js"
+  }
+]);
